a3db9054bf7b10771341a6d5b2be364785d1f624
seanbrant <brant.sean@gmail.com>
2010-05-20 13:51:31 -0500

    test models

--- filch/tests/models.py
+++ filch/tests/models.py
@@ -1,6 +1,9 @@
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from filch.fields import DenormManyToManyField
+from filch.managers import GenericResolutionManager
 
 
 class Location(models.Model):
@@ -17,3 +20,42 @@ class Person(models.Model):
     groups = models.ManyToManyField(Group)
     group_list = DenormManyToManyField('groups',
         attrs=('name', 'location__name'))
+
+
+class Slot(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class HomepageItem(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    order = models.PositiveIntegerField()
+    slot = models.ForeignKey(Slot)
+
+    objects = GenericResolutionManager()
+
+    class Meta(object):
+        ordering = ('order',)
+
+
+class PublishedManager(models.Manager):
+
+    def get_query_set(self):
+        qs = super(PublishedManager, self).get_query_set()
+        return qs.filter(is_published=True)
+
+
+class Article(models.Model):
+    name = models.CharField(max_length=50)
+    is_published = models.BooleanField(default=True)
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def __unicode__(self):
+        return self.name
+
+
+class Press(models.Model):
+    name = models.CharField(max_length=50)
